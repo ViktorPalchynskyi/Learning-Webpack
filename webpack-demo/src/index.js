161f24fcd9b94d17e8e591ea2d94977a897f9bd6
Viktor <palchinskij19@gmail.com>
2024-09-25 18:53:56 +0300

    25.09.24 - adding scss config

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,6 +1,6 @@
 import { AlertService } from './app/alert.service.js';
 import { run } from './app/app.js';
 import { ComponentService } from './app/component.service.js';
-import './main.css';
+import './main.scss';
 
 run(new AlertService(), new ComponentService());
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -10,9 +10,9 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
             },
-        ],
+        ], 
     },
 };
