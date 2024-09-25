f812ae9052a4a3d96b7413076d388bb1c8e3da2f
Viktor <palchinskij19@gmail.com>
2024-09-25 19:11:26 +0300

    25.09.24 - cache busting and plugins

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('node:path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'main.[contenthash].js',
         path: path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -13,6 +14,9 @@ module.exports = {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
-        ], 
+        ],
     },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/template.html'
+    })],
 };
