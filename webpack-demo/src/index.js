8b44f19052a19556a7565f5c327ba70fa3943af9
Viktor <palchinskij19@gmail.com>
2024-09-25 18:43:42 +0300

    25.09.24 - adding css config

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,5 +1,6 @@
 import { AlertService } from './app/alert.service.js';
 import { run } from './app/app.js';
 import { ComponentService } from './app/component.service.js';
+import './main.css';
 
 run(new AlertService(), new ComponentService());
