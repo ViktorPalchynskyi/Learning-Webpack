3404262848505df42d553e59b321192ab5f2c888
Viktor <palchinskij19@gmail.com>
2024-09-25 20:24:45 +0300

    25.09.24 - html-loader, file-loader and Clean Webpack Plugin

--- webpack-demo/webpack.common.js
+++ webpack-demo/webpack.common.js
@@ -8,9 +8,25 @@ module.exports = {
                 test: /\.scss$/,
                 use: ['style-loader', 'css-loader', 'sass-loader'],
             },
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[hash].[ext]',
+                        outputPath: 'imgs',
+                    },
+                },
+            },
         ],
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/template.html'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
 };
